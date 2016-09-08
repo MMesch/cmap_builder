60cbf06748e900367180c44d744bb41fd03ae979
MMesch <MMesch@users.noreply.github.com>
2016-09-08 13:49:12 +0200

    simpler example

--- README.md
+++ README.md
@@ -9,13 +9,25 @@ blender colormap builder
 
 ## Example
 ```
-from colormap2d import imshow2d
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+from colormap2d import imshow2d
 
-data = np.random.normal(size=(2, 10, 10))
+# complex input data
+x = np.linspace(-5, 5, 100)
+regrid, imgrid = np.meshgrid(x, x)
+zgrid = regrid + 1j * imgrid
 
-imshow2d(data)
+complex_function = (zgrid ** 2 - 2.) * (zgrid - 1 - 1j) ** 2 /\
+                   (zgrid + 2j) / (zgrid**2 - 5 - 2j)
+
+
+# assemble [2, nwidth, nheight] array
+data = np.array([np.angle(complex_function),
+                 np.log(np.abs(complex_function))])
+
+# plot to screen
+imshow2d(data, cmap2d='wheel')
 plt.show()
 ```
 
--- colormap2d/example/example1.py
+++ colormap2d/example/example1.py
@@ -6,21 +6,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 from colormap2d import imshow2d
 
-# generate complex input data
-npts_real = 400
-npts_imag = 400
-realaxis = np.linspace(-4, 4, npts_real)
-imagaxis = np.linspace(-3, 3, npts_imag)
-regrid, imgrid = np.meshgrid(realaxis, imagaxis)
-
+# complex input data
+x = np.linspace(-5, 5, 100)
+regrid, imgrid = np.meshgrid(x, x)
 zgrid = regrid + 1j * imgrid
+
 complex_function = (zgrid ** 2 - 2.) * (zgrid - 1 - 1j) ** 2 /\
                    (zgrid + 2j) / (zgrid**2 - 5 - 2j)
 
+
 # assemble [2, nwidth, nheight] array
-data = np.empty((2, npts_imag, npts_real))
-data[1] = np.log(np.abs(complex_function))
-data[0] = np.angle(complex_function)
+data = np.array([np.angle(complex_function),
+                 np.log(np.abs(complex_function))])
 
 # plot to screen
 imshow2d(data, cmap2d='wheel')
